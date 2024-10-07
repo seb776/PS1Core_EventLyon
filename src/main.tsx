e3d33d3c5b05359dc54072872031745ff8859280
seb776 <sebastien.maire@epitech.eu>
2024-10-07 22:37:06 +0200

    Added logos

--- src/main.tsx
+++ src/main.tsx
@@ -6,18 +6,18 @@ import { Canvas } from '@react-three/fiber'
 function MainCover() {
   return <div className='absolute w-full h-full flex flex-row'>
       <div className='absolute h-full right-0 w-[10%] bg-white flex flex-col'>
-        <div className='h-[27.3%] w-full bg-black flex flex-col'>
+        <div className='h-[31%] w-full bg-black flex flex-col'>
           <div className='w-[70%] aspect-square bg-white' style={{margin: '15px auto'}}>
             <div className='h-full w-full' style={{backgroundImage: 'url(gif-sans-marge.gif)', backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}></div>
           </div>
-          <p className='text-white rotate-90 mt-[2.5vh] text-nowrap' style={{fontFamily: 'Playstation', fontSize: '2.7vh', letterSpacing: '0.2vh'}}>Grnd Zero</p>
+          <p className='text-white rotate-90 mt-[2.5vh] text-nowrap' style={{fontFamily: 'Playstation', fontSize: '2.7vh', letterSpacing: '0.2vh'}}>Grrrnd Zero</p>
         </div>
         <div className='h-full w-full'>
-        <p className='text-black rotate-90 mt-[5vh] text-nowrap' style={{fontFamily: 'Playstation', fontSize: '2.7vh', letterSpacing: '0.2vh'}}>Grnd Zero PARTY</p>
+        <p className='text-black rotate-90 mt-[5vh] text-nowrap' style={{fontFamily: 'Playstation', fontSize: '2.7vh', letterSpacing: '0.2vh'}}>60 av de bohlen 69120 Vaulx en Velin @20h</p>
 
         </div>
       </div>
-      <div className='flex justify-center items-center absolute' style={{width: '30vh', backgroundImage: 'url(./Stamp.png)', backgroundSize: 'contain', backgroundRepeat: 'no-repeat', aspectRatio: '1.77'}}>
+      <div className='top-[70vh] right-[5vh] flex justify-center items-center absolute' style={{transform: 'rotate(10deg)', width: '30vh', backgroundImage: 'url(./Stamp.png)', backgroundSize: 'contain', backgroundRepeat: 'no-repeat', aspectRatio: '1.77'}}>
         <div className='text-white text-center' style={{fontSize: '2vh', fontWeight: 'bold'}}>
         vernissage 
         <br/>
