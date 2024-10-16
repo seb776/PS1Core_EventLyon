b0e7f41de6347b46fc89bb9591e495e9e09bffae
seb776 <sebastien.maire@epitech.eu>
2024-10-16 13:24:07 +0200

    Bigger grrrnd zero title + missing release party

--- src/main.tsx
+++ src/main.tsx
@@ -6,14 +6,14 @@ import { Canvas } from '@react-three/fiber'
 function MainCover() {
   return <div className='absolute w-full h-full flex flex-row'>
       <div className='absolute h-full right-0 w-[10%] bg-white flex flex-col'>
-        <div className='h-[31%] w-full bg-black flex flex-col'>
+        <div className='h-[54%] w-full bg-black flex flex-col'>
           <div className='w-[70%] aspect-square bg-white' style={{margin: '15px auto'}}>
             <div className='h-full w-full' style={{backgroundImage: 'url(gif-sans-marge.gif)', backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}></div>
           </div>
-          <p className='text-white rotate-90 mt-[2.5vh] text-nowrap' style={{fontFamily: 'Playstation', fontSize: '2.7vh', letterSpacing: '0.2vh'}}>Grrrnd Zero</p>
+          <p className='text-white rotate-90 mt-[1vh] text-nowrap' style={{fontFamily: 'Playstation', fontSize: '5vh', letterSpacing: '0.2vh'}}>Grrrnd Zero</p>
         </div>
         <div className='h-full w-full'>
-        <p className='text-black rotate-90 mt-[5vh] text-nowrap' style={{fontFamily: 'Playstation', fontSize: '2.7vh', letterSpacing: '0.2vh'}}>20 ans du Grrrnd Zero</p>
+        <p className='text-black rotate-90 mt-[3vh] text-nowrap' style={{fontFamily: 'Playstation', fontSize: '5vh', letterSpacing: '0.2vh'}}>20 ans du Grrrnd Zero</p>
 
         </div>
       </div>
@@ -27,13 +27,21 @@ function MainCover() {
         </div>
       </div>
 
+      <div className='absolute z-50 top-[63vh] left-[40vh] flex justify-center items-center ' style={{transform: 'rotate(-3deg)', width: '22vh', backgroundImage: 'url(./Stamp2.png)', backgroundSize: 'contain', backgroundRepeat: 'no-repeat', aspectRatio: '1.77', filter: "drop-shadow(0px 0px 5px rgba(0, 0, 0, 0.5))"}}>
+        <div className='text-white text-center' style={{fontSize: '2.5vh',fontFamily: 'Bangers',letterSpacing: '0.3vw', fontWeight: 'bold'}}>
+        Release 
+        <br/>
+        party
+        </div>
+      </div>
+
       <div className='absolute bottom-[5vh] right-[5vh] flex justify-center items-center ' style={{transform: 'rotate(-10deg)', width: '17vh', aspectRatio: '2.33',filter: "drop-shadow(0px 0px 5px rgba(0, 0, 0, 0.5))" }}>
         <div className='absolute w-full h-full' style={{backgroundImage: 'url(./PriceTag.png)', backgroundSize: 'contain', backgroundRepeat: 'no-repeat', filter: 'brightness(50%) saturate(100%) invert(69%) sepia(13%) saturate(2710%) hue-rotate(162deg) brightness(101%) contrast(94%)'}}></div>
         <div className='absolute  text-center opacity-70 text-gray-900' style={{transform:'rotate(4deg)', filter: 'blur(0.05vh)', fontSize: '2.5vh', fontWeight: 'bold'}}>
         ± €8.00
         </div>
       </div>
-      <div className='absolute bottom-[2vh] left-[2vh] flex flex-col bg-white w-[25vh]'>
+      <div className='absolute bottom-[2vh] left-[2vh] flex flex-col bg-white w-[27vh]'>
         <div className='uppercase font-normal  border-solid bg-black px-[0.5vh] text-white border-[0.2vh] border-black' style={{fontSize: "2vh", fontFamily: 'Roboto'}}>
           Informations
         </div>
@@ -41,11 +49,12 @@ function MainCover() {
           Ouverture des portes a 20h
         </div>
         <div className='border-solid leading-none border-[0.2vh] border-black px-[0.5vh] py-[0.2vh]' style={{fontSize: "2vh", fontFamily: 'Roboto'}}>
+          Grrrnd Zero <br/>
           60 av de bohlen 69120 Vaulx en Velin
         </div>
       </div>
 
-      <div className='absolute bottom-[2vh] left-[29vh] flex flex-col bg-white w-[15vh]'>
+      <div className='absolute bottom-[2vh] left-[31vh] flex flex-col bg-white w-[15vh]'>
         <div className='uppercase font-normal  border-solid bg-black px-[0.5vh] text-white border-[0.2vh] border-black' style={{fontSize: "2vh", fontFamily: 'Roboto'}}>
           Visuels
         </div>
